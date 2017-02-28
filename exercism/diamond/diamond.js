327ea6c22a45b21c4ca7b839081f21417880ff5d
thelady <dihaynes77@gmail.com>
2017-02-28 11:25:46 -0800

    created diamond.js and all tests passed

---
+++ exercism/diamond/diamond.js
@@ -0,0 +1,12 @@
+var Diamond = function () {}
+
+Diamond.prototype.makeDiamond = function(letter){
+  if(letter === 'A') {
+    return 'A\n'
+  } else {
+    return result
+  }
+}
+
+module.exports = Diamond
+
